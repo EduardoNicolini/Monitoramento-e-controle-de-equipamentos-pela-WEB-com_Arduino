4019a66f6218ded031f83b9bda73247df4ff7526
Eduardo Nicolini <dunicolini@gmail.com>
2022-07-04 02:10:10 -0300

    Update salva_bd.php

--- salva_bd.php
+++ salva_bd.php
@@ -8,8 +8,7 @@
 		
 	
 	$sql = " INSERT INTO sensores (amper, watts, kwh, data) VALUES (:amper, :watts, :kwh, :data) ";
-	//$sql = " INSERT INTO  sensores SET amper = '$amper', quantidade = '$potencia', kwh = $kwh, data = current_date()"; 
-
+	
 	$stmt = $PDO->prepare($sql);
 
 	$stmt->bindParam(':amper', $amper); 
