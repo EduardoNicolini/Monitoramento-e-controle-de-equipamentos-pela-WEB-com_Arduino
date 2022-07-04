ba3fc11de518a338b30b9e0cfdf0a48129d67fe4
Eduardo Nicolini <dunicolini@gmail.com>
2022-07-04 02:10:42 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -6,7 +6,7 @@
 	// COMANDO try TENTA FAZER UMA CONEXÃO COM O Bando de Dados
 	try {
 
-		$PDO = new PDO("mysql:host=localhost:3308;dbname=sensor_arduino;charset=utf8", $usuario, $senha);
+		$PDO = new PDO("mysql:host=localhost;dbname=sensor_arduino;charset=utf8", $usuario, $senha);
 
 	}catch(PDOException $e){ // Vamos criar uma exceção com PDOExecption
 	// COMANDO catch CASO NÃO CONSIGA UMA CONEXÃO COM O BD MOSTRA UM ERRO
